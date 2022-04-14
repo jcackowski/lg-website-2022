305d54a9afd0b8c23624a0caabbdf59a79989d72
jcackowski <imjuon@gmail.com>
2022-04-13 23:19:48 -0400

    hero js customizations

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // global reset
-import './global.style.scss';
+// import './global.style.scss';
 // sass
-import './src/scss/main.scss';
+// import './src/scss/main.scss';
 // javascript
-import './src/js/test';
+import './src/js/hero/noise.min.js';
+import './src/js/hero/util.js';
+import './src/js/hero/shift.js';
