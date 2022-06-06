0b1c6db6025518dd2a60da435db37e0c106e5563
jcackowski <imjuon@gmail.com>
2022-06-05 21:27:40 -0400

    package cleanup remove vite

--- main.js
+++
@@ -1,8 +0,0 @@
-// global reset
-// import './global.style.scss';
-// sass
-// import './src/scss/main.scss';
-// javascript
-import './src/js/hero/noise.min.js';
-import './src/js/hero/util.js';
-import './src/js/hero/shift.js';
